9b02d19135584e1f86e92098e2d64c5f7e8fc3d0
brotherwook <jahanda@naver.com>
2020-06-11 18:35:42 +0900

    최초의 프로젝트

---
+++ src/main/java/com/mycompany/project/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.mycompany.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping("/main.do")
+	public String main() {
+		LOGGER.info("실행");
+		return "home/main";
+	}
+
+	@RequestMapping("/exam19_mqtt.do")
+	public String exam19_mqtt() {
+		LOGGER.info("실행");
+		return "home/exam19_mqtt";
+	}
+}
