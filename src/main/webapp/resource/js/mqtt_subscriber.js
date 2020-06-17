d4895e5661d23db58749c04dddf84b3828eb4579
brotherwook <jahanda@naver.com>
2020-06-17 11:44:57 +0900

    mqtt_subscriber js파일로 만들어버림

--- src/main/webapp/resource/js/mqtt_subscriber.js
+++ src/main/webapp/resource/js/mqtt_subscriber.js
@@ -1,8 +1,9 @@
-// Subscriber Connection
-subscriber = new Paho.MQTT.Client(location.hostname, 61614, new Date().getTime().toString());
-subscriber.onMessageArrived = onMessageArrived;
-subscriber.connect({onSuccess:onSubscriberConnect});
-
+$(function() {
+	// Subscriber Connection
+	subscriber = new Paho.MQTT.Client(location.hostname, 61614, new Date().getTime().toString());
+	subscriber.onMessageArrived = onMessageArrived;
+	subscriber.connect({onSuccess:onSubscriberConnect});
+});
 
 //called when a message arrives
 function onMessageArrived(message) {
